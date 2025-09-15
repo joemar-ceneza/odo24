43646461a3cdeff09f460f4932644036d08fe23b
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-16 02:29:12 +0800

    feat: add IodScopeSection with HeroSection and IodQuoteSection components

---
+++ src/app/oferta/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+import IodQuoteSection from "@/components/QuoteSection";
+
+export default function IodScopeSection() {
+  return (
+    <section className="text-gray-500">
+      <HeroSection />
+      <IodQuoteSection />
+    </section>
+  );
+}
