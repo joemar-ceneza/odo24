711fb3e9c9d1f1bd7981f54a68548458f22a8e94
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-17 02:40:02 +0800

    fix: update IodQuoteSection to use iodScope data and improve layout

--- src/components/IodScopeSection.tsx
+++ src/components/IodScopeSection.tsx
@@ -1,10 +1,10 @@
-import { iodQuote } from "@/lib/iod-data";
+import { iodScope } from "@/lib/iod-data";
 import Image from "next/image";
 
 export default function IodQuoteSection() {
   return (
-    <section className="bg-sky-900 text-white">
-      <div className=" flex justify-center items-center mx-auto pb-20">
+    <section className="bg-sky-950 text-white">
+      <div className="max-w-full w-[1050px] flex justify-between items-center mx-auto">
         <Image
           src="/images/man-with-documents.png"
           alt="Zespół IOD"
@@ -12,8 +12,17 @@ export default function IodQuoteSection() {
           height={500}
           className="scale-x-[-1] -mt-10"
         />
-        <div className="max-w-96">
-          <p className="text-2xl italic">{iodQuote}</p>
+        <div className="max-w-3/4">
+          <p className="text-3xl font-bold">{iodScope.heading}</p>
+          <p className="py-3">{iodScope.description}</p>
+          <ul className="space-y-3 py-3">
+            {iodScope.bullets.map((item, idx) => (
+              <li key={idx} className="flex items-center gap-3">
+                <span className="text-white text-5xl">•</span>
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
