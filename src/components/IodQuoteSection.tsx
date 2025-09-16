9055be1293549fbe3bed9006736c4f4110d09ae7
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-17 02:40:14 +0800

    fix: update IodQuoteSection styles for improved layout

--- src/components/IodQuoteSection.tsx
+++ src/components/IodQuoteSection.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function IodQuoteSection() {
   return (
-    <section className="bg-sky-900 text-white ">
-      <div className=" flex justify-center items-center mx-auto pt-20">
+    <section className="bg-sky-950 text-white ">
+      <div className=" flex justify-center items-center mx-auto pt-10">
         <div className="max-w-96">
           <p className="text-2xl italic">{iodQuote}</p>
         </div>
