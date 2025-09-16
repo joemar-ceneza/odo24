213f55ce6d4a1f829ceb7bbdefcd3e7f5956a20c
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-17 01:31:55 +0800

    feat: add IodOutSourcingSection and IodScopeSection components to OutSourcing page

--- src/app/oferta/[slug]/page.tsx
+++ src/app/oferta/[slug]/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import IodQuoteSection from "@/components/QuoteSection";
+import IodOutSourcingSection from "@/components/IodOutSourcingSection";
+import IodScopeSection from "@/components/IodScopeSection";
 
-export default function IodScopeSection() {
+export default function OutSourcing() {
   return (
-    <section className="text-gray-500">
+    <section className="text-gray-600">
       <HeroSection />
       <IodQuoteSection />
+      <IodOutSourcingSection />
+      <IodScopeSection />
     </section>
   );
 }
