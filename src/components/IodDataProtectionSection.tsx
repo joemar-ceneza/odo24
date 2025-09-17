fa8809cd94583af0f6dba26aabf024a9928e288d
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-18 03:02:47 +0800

    feat: add IodDataProtectionSection component

---
+++ src/components/IodDataProtectionSection.tsx
@@ -0,0 +1,32 @@
+import { iodDataProtection } from "@/lib/iod-data";
+import Image from "next/image";
+
+export default function IodDataProtectionSection() {
+  return (
+    <section>
+      <div className="max-w-[1030px] flex justify-between items-center mx-auto">
+        <div className="">
+          <p className="text-3xl font-bold text-gray-600 my-5">{iodDataProtection.heading}</p>
+          <p className="my-5">{iodDataProtection.description}</p>
+          <ul className="space-y-4">
+            {iodDataProtection.items.map((item, idx) => (
+              <li key={idx} className="flex items-center ml-5">
+                <div className="min-w-[30px] h-[30px] bg-[#b40057] text-white font-bold flex items-center justify-center mr-4">
+                  {String(idx + 1).padStart(2, "0")}
+                </div>
+                <p>{item}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Image
+          src="/images/man-giving-gdrp-circle.png"
+          alt="man-giving-gdrp-circle"
+          width={500}
+          height={500}
+          className="mt-16"
+        />
+      </div>
+    </section>
+  );
+}
