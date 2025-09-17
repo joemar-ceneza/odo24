8c40824a384f6df1ab91a8241da747e87dc29d51
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-18 01:49:13 +0800

    feat: add IodDataProtectionSection to OutSourcing page and update layout wrapper

--- src/app/oferta/[slug]/page.tsx
+++ src/app/oferta/[slug]/page.tsx
@@ -3,15 +3,17 @@ import IodQuoteSection from "@/components/IodQuoteSection";
 import IodOutSourcingSection from "@/components/IodOutSourcingSection";
 import IodScopeSection from "@/components/IodScopeSection";
 import IodOdo24ServiceSection from "@/components/IodOdo24ServiceSection";
+import IodDataProtectionSection from "@/components/IodDataProtectionSection";
 
 export default function OutSourcing() {
   return (
-    <section className="text-gray-500">
+    <main className="text-gray-500">
       <HeroSection />
       <IodQuoteSection />
       <IodOutSourcingSection />
       <IodScopeSection />
       <IodOdo24ServiceSection />
-    </section>
+      <IodDataProtectionSection />
+    </main>
   );
 }
