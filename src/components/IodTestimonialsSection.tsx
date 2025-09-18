d6f2d23761617002c2a8070011e3d5f433abc055
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-19 02:37:30 +0800

    feat: add IodTestimonialsSection component to display testimonials

---
+++ src/components/IodTestimonialsSection.tsx
@@ -0,0 +1,49 @@
+import { iodTestimonialsHeader, iodTestimonials } from "@/lib/iod-data";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function IodTestimonialsSection() {
+  return (
+    <section>
+      <div className="max-w-[1060px] mx-auto">
+        <h2 className="text-3xl text-gray-600 font-bold my-5">{iodTestimonialsHeader}</h2>
+        <div className="grid grid-cols-2 gap-6 py-7">
+          {iodTestimonials.map((testimonial, idx) => (
+            <article key={idx} className="bg-gray-200 rounded-3xl p-5">
+              <div className="flex justify-end items-center my-5">
+                <p>
+                  {testimonial.name} -{" "}
+                  <Link href={testimonial.companyUrl} className="text-sky-500">
+                    {testimonial.company}
+                  </Link>
+                </p>
+
+                <Image src={testimonial.avatar} alt="" width={60} height={60} className="rounded-full ml-5" />
+              </div>
+              <hr className="border-t border-white my-5" />
+              <div>
+                <h3 className="text-lg font-bold my-3">{testimonial.subtitle}</h3>
+                <p className="italic">{testimonial.quote}</p>
+              </div>
+              <div className="flex my-7">
+                <p>
+                  <strong>{testimonial.servicesLabel}</strong>
+                </p>
+                <ul className="flex">
+                  {testimonial.services.map((service, idx) => (
+                    <li key={idx} className="flex">
+                      <Image src="/images/icon-check.svg" alt="check" width={25} height={25} className="mx-1" />
+                      <Link href={service.url} className="text-sky-500 mr-5">
+                        {service.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
