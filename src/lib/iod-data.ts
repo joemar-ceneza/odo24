2f70bccc1de27eb4c6e4425b8fcff8c09aaf0442
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-20 01:14:20 +0800

    feat: add LogoSlider interface and logoSlider data object

--- src/lib/iod-data.ts
+++ src/lib/iod-data.ts
@@ -71,6 +71,11 @@ export interface IodTestimonialsSection {
   }[];
 }
 
+export interface LogoSlider {
+  src: string;
+  alt: string;
+}
+
 export const iodHero: HeroSection = {
   heading: "Outsourcing IOD – Outsourcing funkcji Inspektora Ochrony Danych",
   description:
@@ -250,6 +255,113 @@ export const iodTestimonials: IodTestimonialsSection[] = [
       { label: "Outsourcing IOD", url: "/uslugi/outsourcing-iod" },
       { label: "Wdrożenie RODO", url: "/uslugi/wdrozenie-rodo" },
     ],
+  },
+];
+
+export const logoSlider: LogoSlider[] = [
+  {
+    src: "/images/logo-slider/1.png",
+    alt: "1",
+  },
+  {
+    src: "/images/logo-slider/2.png",
+    alt: "2",
+  },
+  {
+    src: "/images/logo-slider/3.png",
+    alt: "3",
+  },
+  {
+    src: "/images/logo-slider/4.png",
+    alt: "4",
+  },
+  {
+    src: "/images/logo-slider/5.png",
+    alt: "5",
+  },
+  {
+    src: "/images/logo-slider/6.png",
+    alt: "6",
+  },
+  {
+    src: "/images/logo-slider/9.png",
+    alt: "9",
+  },
+  {
+    src: "/images/logo-slider/10.png",
+    alt: "10",
+  },
+  {
+    src: "/images/logo-slider/11.png",
+    alt: "11",
+  },
+  {
+    src: "/images/logo-slider/12.png",
+    alt: "12",
+  },
+  {
+    src: "/images/logo-slider/13.png",
+    alt: "13",
+  },
+  {
+    src: "/images/logo-slider/14.png",
+    alt: "14",
+  },
+  {
+    src: "/images/logo-slider/15.png",
+    alt: "15",
+  },
+  {
+    src: "/images/logo-slider/16.png",
+    alt: "16",
+  },
+  {
+    src: "/images/logo-slider/17.png",
+    alt: "17",
+  },
+  {
+    src: "/images/logo-slider/18.png",
+    alt: "18",
+  },
+  {
+    src: "/images/logo-slider/21.png",
+    alt: "21",
+  },
+  {
+    src: "/images/logo-slider/22.png",
+    alt: "22",
+  },
+  {
+    src: "/images/logo-slider/23.png",
+    alt: "23",
+  },
+  {
+    src: "/images/logo-slider/24.png",
+    alt: "24",
+  },
+  {
+    src: "/images/logo-slider/25.png",
+    alt: "25",
+  },
+  {
+    src: "/images/logo-slider/26.png",
+    alt: "26",
+  },
+  {
+    src: "/images/logo-slider/27.png",
+    alt: "27",
+  },
+  {
+    src: "/images/logo-slider/28.png",
+    alt: "28",
+  },
+  {
+    src: "/images/logo-slider/29.png",
+    alt: "29",
+  },
+  {
+    src: "/images/logo-slider/30.png",
+    alt: "30",
   },
 ];
 
