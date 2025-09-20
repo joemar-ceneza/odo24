a8b59b5d38ecb91dc8a6290e9d23de1c5e53c2fc
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-21 02:16:56 +0800

    apply source sans 3 font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 
 const sourceSans3 = Source_Sans_3({
   variable: "--font-source-sans",
-  subsets: ["latin"],
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -30,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${sourceSans3.variable} antialiased`}>
-        {children}
-      </body>
+      <body className={`${sourceSans3.variable} antialiased`}>{children}</body>
     </html>
   );
 }
