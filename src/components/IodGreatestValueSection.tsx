67bb96bce8e17b95a623fc6a2f53ec4c45e68a5f
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-21 06:10:19 +0800

    refractor: update and improve layout spacing

--- src/components/IodGreatestValueSection.tsx
+++ src/components/IodGreatestValueSection.tsx
@@ -5,16 +5,10 @@ export default function IodGreatValue() {
   return (
     <section className="mt-16">
       <div className="max-w-[1060px] mx-auto px-5">
-        <h2 className="text-2xl font-bold text-gray-600 my-5 max-lg:text-center">{iodGreatestValue.heading}</h2>
-        <div className="flex px-5 pt-10 justify-center items-center max-lg:flex-col">
+        <h2 className="text-3xl font-bold text-gray-600 my-5 max-lg:text-center">{iodGreatestValue.heading}</h2>
+        <div className="flex pt-10 justify-center items-start max-lg:flex-col  max-lg:items-center">
           <Image src={iodGreatestValue.image1} alt={iodGreatestValue.alt1} width={400} height={400} />
-          <Image
-            src={iodGreatestValue.image2}
-            alt={iodGreatestValue.alt2}
-            width={650}
-            height={500}
-            className="h-1/2 max-lg:mb10"
-          />
+          <Image src={iodGreatestValue.image2} alt={iodGreatestValue.alt2} width={650} height={500} />
         </div>
       </div>
     </section>
