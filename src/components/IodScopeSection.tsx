38bf649191065059b38b5dc6e434465bb144b450
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-20 23:35:01 +0800

    feat: make IodScopeSection responsive and add horizontal padding

--- src/components/IodScopeSection.tsx
+++ src/components/IodScopeSection.tsx
@@ -1,18 +1,18 @@
 import { iodScope } from "@/lib/iod-data";
 import Image from "next/image";
 
-export default function IodQuoteSection() {
+export default function IodScopeSection() {
   return (
     <section className="bg-sky-950 text-white">
-      <div className="max-w-full w-[1060px] flex justify-between items-center mx-auto">
+      <div className="max-w-full w-[1060px] flex justify-between items-center mx-auto px-5 max-lg:flex-col">
         <Image
           src="/images/man-with-documents.png"
           alt="Zespół IOD"
           width={500}
           height={500}
-          className="scale-x-[-1] -mt-40"
+          className="scale-x-[-1] -mt-48 max-lg:mt-0"
         />
-        <div className="max-w-3/4 py-20">
+        <div className="max-w-3/4 py-20 max-lg:py-8 max-lg:max-w-full">
           <p className="text-3xl font-bold">{iodScope.heading}</p>
           <p className="py-3">{iodScope.description}</p>
           <ul className="space-y-6 py-3">
