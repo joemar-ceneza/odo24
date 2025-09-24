f141d1ba0801e0be74a9baf8705d946a286ab63e
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-09-24 14:32:22 +0800

    fix: change bullets to items

--- src/data/iod/scope.ts
+++ src/data/iod/scope.ts
@@ -6,7 +6,7 @@ export const scope: IodScopeSection = {
   title: "Jaki jest zakres usługi outsourcing funkcji Inspektora Ochrony Danych",
   subText:
     "Outsourcing funkcji Inspektora Ochrony Danych oznacza dla Ciebie stały kontakt z doświadczonymi konsultantami z obszarów prawa, bezpieczeństwa sieci i systemów IT, zarządzania ryzykiem i bezpieczeństwa fizycznego. Usługa ta to również:",
-  bullets: [
+  items: [
     "Dostęp do narzędzi informatycznych, które pozwalają wykazać zgodność z RODO. Będziesz móc korzystać także z platformy e-learningowej.",
     "Realizacja lub wsparcie w realizacji wielu obowiązków, które spoczywają bezpośrednio na organizacji.",
     "Szybka reakcja na Twoje bieżące potrzeby.",
